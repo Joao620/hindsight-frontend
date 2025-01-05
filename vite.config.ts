99eb883190e30dd6f8f1a04485e4374d09108554
João Carlos <jcarlos.paranhos@gmail.com>
2025-01-05 15:51:29 -0300

    renaming the output index file

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,26 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const renameIndexPlugin = (newFilename: string): import('vite').Plugin => {
+  return {
+    name: 'renameIndex',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      const indexHtml = bundle['index.html']
+      indexHtml.fileName = newFilename
+      console.log('renaming index.html to', indexHtml.fileName)
+    },
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), renameIndexPlugin('404.html')],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.html'
+      }
+    }
+  }
 });
