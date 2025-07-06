1de2077052a86009e7dbf6b76ac3057c0ccfe114
João Carlos <jcarlos.paranhos@gmail.com>
2025-07-06 15:33:20 -0300

    new constants format

--- src/contants.ts
+++ src/contants.ts
@@ -1,4 +1,21 @@
-//export const SERVER_URL = "localhost:5000"
-export const SERVER_URL = "hindsight-c49366b607ea.herokuapp.com"
-export const WEBSOCKET_PROTOCOL = "wss";
-export const HTTP_PROTOCOL = "https";
+const local = true
+
+let SERVER_URL: string
+let WEBSOCKET_PROTOCOL: string
+let HTTP_PROTOCOL: string
+
+if (local){
+    SERVER_URL = "localhost:5000"
+    WEBSOCKET_PROTOCOL = "ws"
+    HTTP_PROTOCOL = "http"
+} else {
+    SERVER_URL = "https://hindsight-c49366b607ea.herokuapp.com"
+    WEBSOCKET_PROTOCOL = "wss"
+    HTTP_PROTOCOL = "https"
+}
+
+export {
+    SERVER_URL,
+    WEBSOCKET_PROTOCOL,
+    HTTP_PROTOCOL
+}
